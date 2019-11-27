4bf0c718e5e575d82d5743f29a2f9caef66a88dd
Germanicus Louis <Louis.germanicus@outlook.com>
2019-11-27 14:28:42 +0100

    final

--- TP4/routes/movies.js
+++ TP4/routes/movies.js
@@ -6,12 +6,22 @@ var _ = require('lodash');
 API_KEY = "b0f1bc0d";
 API_URL = "http://www.omdbapi.com/";
 
-var ListMovies = [];
+var ListMovies = [{
+  id: "30",
+  movie: "yoyo",
+  yearOfRelease: "1990",
+  duration: "67", // en minutes,
+  actors: ["Paul", "Jacques"],
+  poster: "oui", // lien vers une image d'affiche,
+  boxOffice: "5000", // en USD$,
+  rottenTomatoesScore: "7",
+ }]
+
 
 /* GET movies. */
 router.get('/', function(req, res, next) {
     res.status(200).json({
-      ListMovies
+      ListMovies : ListMovies
     })
   });
 
@@ -21,29 +31,30 @@ router.get('/:id', (req, res) => {
   const selec = _.find(ListMovies, ["id", id]);
   console.log("title", id, selec);
   res.status(200).json({
-    message: 'trouvé',
+    message: 'Voici le film :',
     selec
   });
 });
 
 /* PUT movie */
-router.put("/", (req, res) => {
-  const {title} = req.body;
+router.put("/:title", (req, res) => {
+  const {title} = req.params;
   axios.get(`${API_URL}?t=${title}&apikey=${API_KEY}`).then(({data}) => {
     const id = _.uniqueId();
-    const title = data.Title;
-    const annee = data.Year;
+    const titre = data.Title;
+    const an = data.Year;
     const temps = data.Runtime;
     const acteur = data.Actors;
     const poster = data.Poster;
     const office = data.BoxOffice;
-    const notes = data.Ratings[1].Value;
-    const list ={"id":id, "movie":title, "yearOfRelease": annee, "duration": temps, "actors": acteur, "poster": poster, "boxOffice": office, "rottenTomatoesScore": notes};
-    ListMovies.push(list)
-  });
+    const rate = data.Ratings[1].Value;
+    const list ={"id":id, "movie":titre, "yearOfRelease": an, "duration": temps, "actors": acteur, "poster": poster, "boxOffice": office, "rottenTomatoesScore": rate};
+    ListMovies.push(list);
   res.status(200).json ({
-    message: `${title} ajouté à la base de donnée`
+    message: `${title} ajouté à la base de donnée`,
+    movie: title,
   });
+});
 });
 
 /* DELETE movie */
@@ -51,18 +62,18 @@ router.delete('/:id', (req, res) => {
   const {id} = req.params;
   _.remove(ListMovies, ["id", id]);
   res.status(200).json({
-    message: '${id} retiré'
+    message: `${id} retiré `
   })
 })
 
 /* POST movie */
-router.post('/:id', (req, res) => {
+router.post('/:id/:title', (req, res) => {
   const{id} = req.params;
-  const{movie} = req.body;
+  const{title} = req.params;
   const update = _.find(ListMovies, ["id", id]);
-  update.movie = movie;
+  update.movie = title;
   res.status(200).json({
-    message: '${id} update'
+    message: `${id} update `
   })
 })
 
